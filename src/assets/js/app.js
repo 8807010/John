38a633908e31398b98a38a20a9509459c9cf6de8
Artiom_Sokolov <sokolova-net@mail.ru>
2022-02-11 19:22:22 +0300

    js complited

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,8 +5,14 @@ fun = (a) => {
 
 let menu = document.querySelector('.header__menu');
 let burger = document.querySelector('.header__burger');
-// let close = document.querySelector('.close');
+let close = document.querySelector('.close');
 
 burger.addEventListener('click', () => {
     menu.classList.toggle('active');
+
+    if (menu.classList.contains('active')) {
+        burger.classList.add('close')
+    } else {
+        burger.classList.remove('close')
+    }
 });
